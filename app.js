27f7c5b51e00eb7a29ba8f3b95e6a0d1eba70a67
owen <owen.rim@edwardsvacuum.com>
2023-04-23 18:33:26 +0900

    #2.2 Line Width

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPaining = false;
 function onMove(event) {
   if (isPaining) {
@@ -11,6 +11,7 @@ function onMove(event) {
     ctx.stroke();
     return;
   }
+
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -20,9 +21,16 @@ function startPainting() {
 
 function cancelPainting() {
   isPaining = false;
+  ctx.beginPath();
+}
+function onLineWidthChange(event) {
+  console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
