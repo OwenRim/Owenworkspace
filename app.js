ebd1f17a4cb06b2cb01dbd086bec0df2c1e2a067
owen <owen.rim@edwardsvacuum.com>
2023-04-27 19:18:53 +0900

    #2.4 Paint Color part Two

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const modeBtn = document.getElementById("mode-btn");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
@@ -6,6 +10,8 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPaining = false;
+let isFilling = false;
+
 function onMove(event) {
   if (isPaining) {
     ctx.lineTo(event.offsetX, event.offsetY);
@@ -31,7 +37,25 @@ function onLineWidthChange(event) {
 
 function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
 }
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+// function onModeClick() {
+//   if (isFilling) {
+//     isFilling = false;
+//     modeBtn.innerText = "Fill";
+//   } else {
+//     isFilling = true;
+//     modeBtn.innerText = "Draw";
+//   }
+// }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
@@ -40,3 +64,7 @@ canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+//modeBtn.addEventListener("click", onModeClick);
